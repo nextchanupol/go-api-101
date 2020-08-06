6c6ea21ecc584a8e0dd9230e63a7c161c4a4757f
Chanupol Phermphoon <chanupol.gotoceo@gmail.com>
2020-08-06 10:00:26 +0700

    move sql statement to member model

--- pkg/app/member.go
+++ pkg/app/member.go
@@ -1,11 +1,9 @@
 package app
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
-	pgsql12 "github.com/nextchanupol/go-api-101/pkg/database"
 	model "github.com/nextchanupol/go-api-101/pkg/modules/model/profile"
 )
 
@@ -16,28 +14,12 @@ func CreateMember(c echo.Context) (err error) {
 	m := model.InitNewMember()
 
 	if err := c.Bind(m); err != nil {
-		return err
+
+		return c.JSON(http.StatusInternalServerError, err)
 	}
 
-	createStatement := `insert into member 
-												(id, first_name, last_name, email, password)
-											values 
-												($1, $2, $3, $4, $5) returning *`
-
-	statement, err := pgsql12.Db.Prepare(createStatement)
-	defer statement.Close()
-
+	m, err = model.CreateMember(m)
 	if err != nil {
-		log.Printf("Prepare error: %v", err)
-		return err
-	}
-
-	log.Printf("Prepare error: %v %v", m.CreatedAt, m.UpdatedAt)
-
-	_, err = statement.Exec(m.ID, m.FirstName, m.LastName, m.Email, m.Password)
-
-	if err != nil {
-		log.Printf("Exec error: %v", err)
 		return err
 	}
 
--- pkg/modules/model/profile/member.go
+++ pkg/modules/model/profile/member.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"log"
+	"time"
+
+	pgsql12 "github.com/nextchanupol/go-api-101/pkg/database"
+)
 
 // Member struct
 type Member struct {
@@ -23,4 +28,30 @@ func InitNewMember() *Member {
 		CreatedAt: now,
 		UpdatedAt: now,
 	}
+}
+
+// CreateMember creates a new member
+func CreateMember(member *Member) (*Member, error) {
+	createStatement := `insert into member 
+												(id, first_name, last_name, email, password, created_at, updated_at)
+											values 
+												($1, $2, $3, $4, $5, $6, $7) returning *`
+
+	statement, err := pgsql12.Db.Prepare(createStatement)
+	defer statement.Close()
+
+	if err != nil {
+		log.Printf("Prepare error: %v", err)
+		return nil, err
+	}
+
+	log.Printf("Prepare error: %v %v", member.CreatedAt, member.UpdatedAt)
+
+	_, err = statement.Exec(member.ID, member.FirstName, member.LastName, member.Email, member.Password, member.CreatedAt, member.UpdatedAt)
+
+	if err != nil {
+		log.Printf("Exec error: %v", err)
+		return nil, err
+	}
+	return member, nil
 }
